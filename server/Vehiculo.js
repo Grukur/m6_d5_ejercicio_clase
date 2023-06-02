2e549881f49a0d6a4a68b86afbc7193c54a6dd98
Grukur <darold.trench@gmail.com>
2023-06-02 13:02:29 -0400

    update 3

---
+++ server/Vehiculo.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const {v4:uuid} = require('uuid')
+ 
+
+
+class Vehiculo{
+    constructor(id, marca, modelo, asientos){
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.asientos = asientos;
+    }
+    findAllObj(){
+        let resultadoJson = fs.readFileSync(__dirname+'/data.json', 'utf8')
+        let resultado = JSON.parse(resultadoJson)
+        return resultado
+    }
+    findAll(){
+
+        return this.findAllObj().autos
+    }
+    findById(id){
+        let autos = this.findAll()
+        return autos.find(auto => auto.id == id)
+    }
+    findByMarca(marca){
+        let autos = this.findAll()
+        return autos.filter(auto => auto.marca.toLowerCase() == marca.toLowerCase())
+    }
+    save(){
+        let objAuto = {
+            id: uuid().slice(0,6),
+            marca: this.marca,
+            modelo: this.modelo,
+            asientos: this.asientos
+        }
+        let resultado = this.findAllObj()
+        resultado.autos.push(objAuto)
+        fs.writeFileSync(__dirname+'/data.json', JSON.stringify(resultado, null, 4), 'utf8')
+        return objAuto
+    }
+    delete(id){
+        let resultado = this.findAllObj()
+        let encontrado = this.findById(id)
+        if(encontrado){
+            resultado.autos = resultado.autos.filter(auto => auto.id != id)
+            fs.writeFileSync(__dirname+'/data.json', JSON.stringify(resultado, null, 4), 'utf8')
+            return resultado
+        }else{
+            return {message:`El id: ${id} no fue necontrado`}
+        }
+    }
+    update(){
+        let resultado = this.findAllObj()
+        let filtrado = resultado.autos.find(auto => auto.id == this.id)
+        if(filtrado){
+            filtrado.marca = this.marca || filtrado.marca
+            filtrado.modelo = this.modelo || filtrado.modelo
+            filtrado.asientos = this.asientos || filtrado.asientos
+            fs.writeFileSync(__dirname+'/data.json', JSON.stringify(resultado, null, 4), 'utf8')
+            return filtrado
+        } else {
+            return {message:`El id: ${this.id} no fue necontrado`}
+        }
+    }
+}
+
+module.exports = {
+    Vehiculo
+}
+
+/* let auto = new Vehiculo()
+console.log(auto.findAll())
+console.log(auto.findById(4))
+console.log(auto.findByMarca('chiripas'))
+let auto2 = new Vehiculo(1, 'Ferrari', 'F40', 3)
+let resultado = auto2.update()
+console.log(resultado)
+console.log(auto2.findAll()) */
--- server/operaciones.js
+++ server/operaciones.js
@@ -1,27 +1,84 @@
-const fs = require('fs');
+const {Vehiculo} = require('./Vehiculo.js');
 
-class Vehiculo{
-    constructor(id, marca, modelo, asientos){
-        this.id = id;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.asientos = asientos;
+const leerVehiculos = ()=>{
+    let autos = new Vehiculo();
+    console.log(autos.findAll())
+};
+
+const buscarVehiculoPorId = (id)=>{
+    let autos = new Vehiculo()
+    let resultado = autos.findById(id)
+    if(resultado){
+        console.log(resultado);
+    } else {
+        console.log(`vehiculo con id: ${id} no fue encontrado`);
     }
-    findAll(){
-        let resultadoJson = fs.readFileSync(__dirname+'/data.json', 'utf8')
-        let resultado = JSON.parse(resultadoJson)
-        return resultado.autos
+};
+
+const buscarVehiculoPorMarca = (marca)=>{
+    let autos = new Vehiculo()
+    let resultado = autos.findByMarca(marca)
+    if(resultado){
+        console.log(resultado);
+    } else {
+        console.log(`vehiculo con la marca: ${marca} no fue encontrado`);
     }
-    findById(id){
-        let autos = this.findAll()
-        return autos.find(auto => auto.id == id)
-    }
-    findByMarca(marca){
-        let autos = this.findAll()
-        return autos.filter(auto => auto.marca.toLowerCase() == marca.toLowerCase())
-    }
-}
-let auto = new Vehiculo()
-console.log(auto.findAll())
-console.log(auto.findById(4))
-console.log(auto.findByMarca('ferrari'))
+};
+
+const crearVehiculo = (marca, modelo, asientos)=>{
+    let auto = new Vehiculo(undefined, marca, modelo, asientos)
+    let resultado = auto.save()
+    console.log('Vehiculo creado: ',resultado)     
+};
+
+const eliminarVehiculo = (id)=>{
+    let auto = new Vehiculo()
+    let resultado = auto.delete(id)
+    console.log(resultado)    
+};
+
+const modificar = (id, marca, modelo, asientos)=>{
+    console.log(id, marca, modelo, asientos)
+    marca == 'undefined'? marca = undefined:marca=marca
+    modelo == 'undefined'? modelo = undefined:modelo=modelo
+    asientos == 'undefined'? asientos = undefined:asientos=asientos
+    let auto = new Vehiculo(id, marca, modelo, asientos)
+    let resultado = auto.update()
+    console.log(resultado)
+};
+
+let argumentos = process.argv.slice(2);
+let comando = argumentos[0].toLowerCase();
+
+switch(comando){
+    case 'leer':
+        leerVehiculos()
+        break;
+    case 'buscarid':
+        let id = argumentos[1]
+        buscarVehiculoPorId(id)
+        break;
+    case 'buscarmarca':
+        let marca = argumentos[1]
+        buscarVehiculoPorMarca(marca)
+        break;
+    case 'crear':
+        let marcaSave = argumentos[1]
+        let modelo = argumentos[2]
+        let asientos = argumentos[3]
+        crearVehiculo(marcaSave, modelo, asientos)
+        break;
+    case 'eliminar':
+        let idDelete = argumentos[1]
+        eliminarVehiculo(idDelete)
+        break;
+    case 'modificar':
+        let idMod = argumentos[1]
+        let marcaMod = argumentos[2]
+        let modeloMod = argumentos[3]
+        let asientosMod = argumentos[4]
+        modificar(idMod, marcaMod, modeloMod, asientosMod)
+        break;
+    default:
+        console.log('Comando no reconocido')
+};
